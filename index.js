9a61470825ca5e11a6c2f82dd449a274d85f6ea4
Archie Baer <archie@abaer.dev>
2020-07-28 03:51:09 +0100

    Split into files

--- index.js
+++ index.js
@@ -1,68 +1,11 @@
-const axios = require("axios");
-const sharp = require("sharp");
-const fs = require("fs");
-const images = {
-    noUser: fs.readFileSync(`${__dirname}/images/noUser.png`),
-    placeholder: fs.readFileSync(`${__dirname}/images/placeholder.png`)
-};
-
 // Express
 const express = require("express");
-const e = require("express");
 const app = express();
 app.use(require("cors")());
 app.listen(8080, () => console.log("Listening on Express"));
 
 // Get Avatar
-app.get("/:id", async (req, res) => {
-    const {id} = req.params;
-
-    // Get User
-    let user;
-    try {
-        user = (await axios.get(
-            `https://1api.alles.cx/v1/user?id=${encodeURIComponent(id)}`,
-            {
-                auth: {
-                    username: process.env.ALLES_ID,
-                    password: process.env.ALLES_SECRET
-                }
-            }
-        )).data;
-    } catch (err) {}
-
-    // Get Image
-    let image;
-    if (!user) image = images.noUser;
-    else if (!user.avatar) image = images.placeholder;
-    else {
-        try {
-            image = (await axios.get(`https://fs.alles.cx/${user.avatar}`, {
-                responseType: "arraybuffer"
-            })).data;
-        } catch (e) {
-            image = images.placeholder;
-        }
-    }
-
-    // Image Size
-    const size =
-        typeof req.query.size === "string" &&
-        !isNaN(req.query.size) &&
-        Number.isInteger(Number(req.query.size)) &&
-        Number(req.query.size) > 0 &&
-        Number(req.query.size) <= 500
-            ? Number(req.query.size) : 200;
-    
-    // Manipulate Image
-    image = await sharp(image)
-        .resize(size)
-        .png()
-        .toBuffer();
-
-    // Response
-    res.type("image/png").send(image);
-});
+app.get("/:id", require("./getAvatar"));
 
 // 404
 app.use((_req, res) => res.status(404).json({err: "notFound"}));
